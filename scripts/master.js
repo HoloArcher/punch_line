bacf13da9f12e561baae4f2e2072086248be3a19
Schiess Josiah <in17scjo@lwb.ch>
2018-01-12 10:02:03 +0100

    -

--- scripts/master.js
+++
@@ -1,9 +0,0 @@
-function f1() {
-  document.getElementById('a1').innerHTML= 'Selfdestruct';
-};
-function f2() {
-document.getElementById('a1').innerHTML='Press Me';
-}
-function functionName() {
-
-}
